93f86cec999b961ee9be24ee6b4fe89eee8007aa
f.zesch <f.zesch@4flow.de>
2015-09-12 22:33:36 +0200

    database initialisation, contains minor errors :)

--- code/create_empty_sqlite.py
+++ code/create_empty_sqlite.py
@@ -13,26 +13,27 @@ def create_tables(db_con):
     :return:
     """
 
-    LOCATIONS = ["LAGESO"]
-    USERS = ["Helper"]
-    PW = ["1234"]
+    PLACE_INIT_DATA = ("LAGESO", "Helper", "^[a-zA-Z]{1,1}[0-9]+$", 1, 99)
+    USER_INIT_DATA = ("Helper", "1234", 0)
+
+
 
     INPUT_TABLES = {
     "PLACES" : {"PLACE": "VARCHAR(20)",
-                          "USER": "VARCHAR(10)",
-                          "VALREGEXP": "VARCHAR(99)",
-                          "MIN_LENGTH": "INTEGER",
-                          "MAX_LENGTH": "INTEGER"}
+                "USER": "VARCHAR(10)",
+                "VALREGEXP": "VARCHAR(99)",
+                "MIN_LENGTH": "INTEGER",
+                "MAX_LENGTH": "INTEGER"}
 
 
     "USERS" : { "USER": "VARCHAR(10)",
-                          "PW": "VARCHAR(20)",
-                          "ISADMIN": "BOOLEAN"}
+                "PW": "VARCHAR(20)",
+                "ISADMIN": "BOOLEAN"}
 
     "NUMBERS" : {"NUMBER": "VARCHAR(30)",
-                           "TIME": "TIMESTAMP",
-                           "PLACE": "VARCHAR(20)",
-                           "USER": "VARCHAR(10)"}
+                "TIME": "TIMESTAMP",
+                "PLACE": "VARCHAR(20)",
+                "USER": "VARCHAR(10)"}
 
     }
 
@@ -44,17 +45,24 @@ def create_tables(db_con):
 
             cur = con.cursor()
 
+            # create all tables in loop
             for key in INPUT_TABLES:
 
+                # concat field names and data types for creation string
                 sql_string = "CREATE TABLE " + key + "(" + \
-                             " ".join("") + \
+                             " ".join(
+                                 [" ".join([subkey, INPUT_TABLES[key][subkey]])+"," for subkey in INPUT_TABLES[key]]
+                             )[:-2]+ \
                              ")"
+                # run
                 cur.execute(sql_string)
 
+            # initial record for location and user
+            cur.execute("INSERT INTO PLACES(PLACE, USER, VALREGEXP, MIN_LENGTH, MAX_LENGTH) VALUES (PLACE_INIT_DATA)")
+            cur.execute("INSERT INTO USERS(USER, PW, ISADMIN) VALUES (USER_INIT_DATA)")
 
-    except mdb.Error, e:
-
-        print "Error %d: %s" % (e.args[0],e.args[1])
+    except mdb.Error as e:
+        print("Error %d: %s" % (e.args[0],e.args[1]))
         sys.exit(1)
 
     finally:
