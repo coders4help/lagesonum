9e5fdad8a8bb919d810d97c1ea82d384c6ac7378
f.zesch <f.zesch@4flow.de>
2015-09-14 20:35:34 +0200

    better local handling, should not affect deployment on server

--- lagesonum/__main__.py
+++ lagesonum/__main__.py
@@ -6,4 +6,5 @@ from bottle import run
 
 from bottle_app import application
 
-run(application, host='localhost', port=8080)
+debug(True)
+run(application, host='localhost', port=8080, reloader=True)
