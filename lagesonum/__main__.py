ffdaf7cb7e2c115e6bace17f8ad496ff43295a1a
Christoph Meißner <chris@orderbird.com>
2015-09-14 03:30:00 +0200

    Move sqlite db file out of repository
    When updating the deployed version, the sqlite db should not get lost.

--- lagesonum/__main__.py
+++ lagesonum/__main__.py
@@ -1,7 +1,9 @@
+# coding: utf-8
 
 # Datei zum lokalen testen, PythonAnywhere verwendet bottle_app.py direkt
 
-from bottle_app import application
 from bottle import run
+
+from bottle_app import application
 
 run(application, host='localhost', port=8080)
