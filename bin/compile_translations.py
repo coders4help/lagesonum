9c98b507e44927dcc0cdb4a5d287a4e4ed85b1c5
Peter Palmreuther <pitpalme@yahoo.de>
2015-10-10 14:42:34 +0100

    PO compilation feature
    Added polib based compilation of .po files in locales dir.
    
    .mo files removed from repository and added to .gitignore.
    They're supposed to be compiled on demand and not managed by SCM.

---
+++ bin/compile_translations.py
@@ -0,0 +1,28 @@
+# -*- conding: utf-8 -*-
+
+import os
+import polib
+import sys
+from inspect import getmembers as gm
+
+LOCALES_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../lagesonum/locales'))
+
+if '__main__' == __name__:
+    locale_dir = sys.argv[1] if 1 < len(sys.argv) else LOCALES_DIR
+
+    if not (os.path.exists(locale_dir) and os.path.isdir(locale_dir)):
+        print(u'Locales directory \'{}\' does not exist or is not a directory'.format(LOCALES_DIR))
+        exit(-1)
+
+    i18n_files = []
+    for subdir, dirs, files in os.walk(LOCALES_DIR, followlinks=True):
+        for file in files:
+            if file.endswith('.po'):
+                i18n_files.append({'dir': os.path.relpath(subdir, os.getcwd()), 'file': file})
+
+    for pofile in i18n_files:
+        print(u'Processing {}'.format(pofile['dir']))
+        pofile_name = os.path.join(pofile.get('dir'), pofile['file'])
+        mofile_name = os.path.join(pofile.get('dir'), 'messages.mo')
+        polib.pofile(pofile_name).save_as_mofile(mofile_name)
+        print(u'Compiled {}'.format(pofile['file']))
