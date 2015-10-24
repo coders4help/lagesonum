4b4bd260f58a90583f7e3a79e100f329a5802016
Kai Rollmann <kai.rollmann@googlemail.com>
2015-10-24 19:35:25 +0200

    Add sass support
    - Compile:
        python3 manage.py compile_sass

---
+++ website/management/commands/compile_sass.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import sass
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        # TODO: Make (more) dynamic
+        outfile = settings.BASE_DIR + '/website/static/css/main.css'
+        f = open(outfile, 'w')
+
+        # TODO: Error handling
+        infile = settings.BASE_DIR + '/scss/main.scss'
+        f.write(sass.compile(filename=infile))
+
+        f.close()
