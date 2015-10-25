4e1e66ebff0b9783fa177cc54c1e3f535e9740bd
Kai Rollmann <kai.rollmann@googlemail.com>
2015-10-25 12:08:48 +0100

    Improve the sass and scss pipeline
    - Use sass instead of scss
    - Add css3-mixins (they come very handy also for
      cross-browser-compatibility)
    - Completely import Andre's sass from the new design
    - Start working on the navbar.

--- website/management/commands/compile_sass.py
+++ website/management/commands/compile_sass.py
@@ -13,7 +13,7 @@ class Command(BaseCommand):
         f = open(outfile, 'w')
 
         # TODO: Error handling
-        infile = settings.BASE_DIR + '/website/scss/main.scss'
+        infile = settings.BASE_DIR + '/website/sass/main.sass'
         f.write(sass.compile(filename=infile))
 
         f.close()
